d0e8ea894996ce6cecee8b5e450920167a2542a8
ramezHamdiSa'eed <ramez.hamdi4work@gmail.com>
2024-05-06 06:41:07 -0700

    Initial Commit

---
+++ IOS_ReferenceUITests/IOS_ReferenceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IOS_ReferenceUITestsLaunchTests.swift
+//  IOS_ReferenceUITests
+//
+//  Created by Ramez Hamdi Saeed on 06/05/2024.
+//
+
+import XCTest
+
+final class IOS_ReferenceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
