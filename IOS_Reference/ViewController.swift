56abd39a104a5f441412fd7a049248b992b3e16d
ramezHamdiSa'eed <ramez.hamdi4work@gmail.com>
2024-05-06 07:04:52 -0700

    init viewController with Previews

--- IOS_Reference/ViewController.swift
+++ IOS_Reference/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+        
     }
 
 
+}
+
+struct ViewControllerRepresentable : UIViewControllerRepresentable{
+    
+    typealias UIViewControllerType = ViewController
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        
+        ViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+        //
+    }
+    
+}
+
+struct ViewControllerPreviews : PreviewProvider{
+    static var previews: some View{
+        ViewControllerRepresentable().edgesIgnoringSafeArea(.all)
+    }
 }
 
